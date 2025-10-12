0cb03d8a5b70fdceb404f81a392ab513cd18c5e7
The-Ex-Postman <louis.cavril31@gmail.com>
2025-10-12 19:50:45 +0200

    mongo: modification du path de connexion à mongo

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const app = require('../app');
-const connectMongoDB = require('../config/mongo');
+const connectMongoDB = require('/src/config/mongo');
 
-let mongoReady; // cache de promesse pour réutiliser la connexion
+let mongoReady;
 module.exports = async (req, res) => {
   try {
     if (!mongoReady) mongoReady = connectMongoDB();
     await mongoReady;
-    return app(req, res); // ⚡️ délègue à Express
+    return app(req, res);
   } catch (err) {
     console.error('API init error', err);
     res.status(500).json({ ok: false, message: 'Erreur serveur' });
