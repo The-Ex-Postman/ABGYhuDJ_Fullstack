d73a31605f76bbb1c16baca6dda157c945c4ce58
The-Ex-Postman <louis.cavril31@gmail.com>
2025-10-12 19:46:41 +0200

    server: modification de la procédure de connexion à mongoDB

--- src/server.js
+++ src/server.js
@@ -1,28 +1,7 @@
 //connexion à MongoDB
 require('dotenv').config();
-const app = require('./app');
+const app = require('express')();
 const connectMongoDB = require('./config/mongo');
-
-const PORT = process.env.PORT || 5000;
-
-if (require.main === module) {
-  (async () => {
-    await connectMongoDB();
-    app.listen(PORT, () => {
-      console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
-    });
-  })();
-}
 
 module.exports = app;
 
-// Alternative start server method
-
-const startServer = async () => {
-  await connectMongoDB(); // Connexion Mongo
-  app.listen(PORT, () => {
-    console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
-  });
-};
-
-startServer();
